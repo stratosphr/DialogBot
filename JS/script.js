b30cad99535999d84b90594b86c700287dd37a0c
stratosphr <stratosphr@gmail.com>
2013-02-01 23:20:24 +0100

    Adding javascript and css

---
+++ JS/script.js
@@ -0,0 +1,56 @@
+function refreshChat(list_messages){
+	 var chat = $("#textarea_chat");
+	 chat.val("");
+	 for(var i=0; i<list_messages.length; i++){
+		  if(i == 0) chat.val("You : " + list_messages[i]);
+		  else if(i % 2 == 0) chat.val(chat.val() + "\n\nYou : " + list_messages[i]);
+		  else chat.val(chat.val() + "\n\nBot : " + list_messages[i]);
+	 }
+}
+
+$(document).ready(function(){
+
+	 var list_messages = new Array();
+
+	 $("#input_message").focus();
+
+
+	 $("#input_submit").click(function(event){
+		  event.preventDefault();
+
+		  var chat = $("#textarea_chat");
+		  var new_message = $("#input_message").val();
+
+		  if(new_message != ""){
+			   list_messages.push(new_message);
+			   refreshChat(list_messages);
+			   chat.animate({scrollTop : chat[0].scrollHeight - chat.height()}, 300);
+			   $("#input_message").val("");
+		  }
+		  $("#input_message").focus();
+
+	 });
+
+	 $("#input_clear").click(function(event){
+		  event.preventDefault();
+		  list_messages.splice(0, list_messages.length);
+		  refreshChat(list_messages);
+		  $("#input_message").focus();
+	 });
+
+
+	 $("#input_message").keydown(function(event){
+		  if(event.which == 38){ // Up arrow was pressed
+			   event.preventDefault();
+			   if(list_messages.length > 0){
+					var chat = $("#textarea_chat");
+					last_message = list_messages[list_messages.length - 2];
+					$("#input_message").val(last_message);
+					list_messages.pop();
+					list_messages.pop();
+					refreshChat(list_messages);
+			   }
+		  }
+	 });
+
+});
---
+++ index.php~
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>DialogBot v1.0</title>
+	<script src="JS/jquery.js"></script>
+	<!--<meta http-equiv="refresh" content="0.3">-->
+	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+	<meta charset="UTF-8"/>
+</head>
+
+<body>
+
+	<h1>DialogBot</h1>
+	<form action="index.php" method="post">
+		<label for="input_message">Enter your message here :</label>
+		<input id="input_message" type="text"/>
+		<input id="input_submit" type="submit" value="Send"/>
+		<textarea cols="80" id="textarea_chat" rows="14"></textarea>
+		<input id="input_clear" type="button" value="Clear chat"/>
+		<input id="input_reset_bot" type="button" value="Reset bot"/>
+	</form>
+
+	 <script src="JS/script.js" type="text/javascript"></script>
+
+</body>
+
+</html>
